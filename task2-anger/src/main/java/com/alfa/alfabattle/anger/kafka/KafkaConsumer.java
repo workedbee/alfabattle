55500837be56fa24d5a493e4fe13708854b0d93e
workedbee <d.maklakov@gmail.com>
2020-06-27 15:09:53 +0700

    Kafka is supported.

---
+++ task2-anger/src/main/java/com/alfa/alfabattle/anger/kafka/KafkaConsumer.java
@@ -0,0 +1,23 @@
+package com.alfa.alfabattle.anger.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+    @Autowired
+    public KafkaConsumer() {}
+
+    @KafkaListener(topics = "topicName")
+    public void listenWithHeaders(
+            @Payload String message,
+            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+        System.out.println(
+                "Received Message: " + message
+                        + "from partition: " + partition);
+    }
+}
